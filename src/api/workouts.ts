211434af9624e372f948678d768ed7c80893c4ad
marcinkula1982 <marcinkula@interia.pl>
2026-04-24 10:29:44 +0200

    fix: add x-username to buildAuthHeaders

--- src/api/workouts.ts
+++ src/api/workouts.ts
@@ -5,11 +5,11 @@ import client from './client'
 
 const buildAuthHeaders = (): Record<string, string> => {
   const sessionToken = localStorage.getItem('tcx-session-token')
+  const username = localStorage.getItem('tcx-username')
 
   const headers: Record<string, string> = {}
-  if (sessionToken) {
-    headers['x-session-token'] = sessionToken
-  }
+  if (sessionToken) headers['x-session-token'] = sessionToken
+  if (username) headers['x-username'] = username
   return headers
 }
 
